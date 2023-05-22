cdc49039a487ae14f433355812e7d5760674bb33
jfmarkan <jfmarkan@gmail.com>
2023-05-22 18:14:47 +0200

    EVEN ODD Game done!

--- script.js
+++ script.js
@@ -23,5 +23,35 @@ if (palindromeCheck(straightWord, reverseWord)){
     console.log ('The word ' + wordToVerify + ' is a Palindrome');
     } else {
     console.log ('The word ' + wordToVerify + ' is not a Palindrome');
-    }
+}
 
+
+let evenOddChoice = prompt('Insert if the result will be EVEN or ODD');
+let userNumber = parseInt(prompt('Insert a number between 1 and 5'));
+
+
+function getRandomInt (min, max){
+    const randomNumber =  Math.floor(Math.random()* max) + min;
+    return randomNumber;
+}
+
+function sumNumbers (number1, number2){
+    const result = number1 + number2;
+    return result;
+}
+
+function evenVerify (dividendo){
+    if (dividendo % 2 === 0){
+        return true;
+    };
+    return false;
+}
+
+
+if ((evenVerify(sumNumbers(getRandomInt(1,5), userNumber))) && (evenOddChoice = "EVEN")){
+    console.log('You have won! the final result is EVEN');
+} else if ((!evenVerify(sumNumbers(getRandomInt(1,5), userNumber))) && (evenOddChoice = "ODD")){
+    console.log('You have won! the final result is ODD');
+} else {
+    console.log("Sorry, you've lost");
+}
